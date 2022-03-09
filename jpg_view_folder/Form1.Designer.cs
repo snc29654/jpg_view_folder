0234564f7e11ae86fc55b32e6d48eb09fe34e07b
snc29654 <snc29654@nifty.com>
2022-03-09 12:43:33 +0900

    メモを複数行対応

--- jpg_view_folder/Form1.Designer.cs
+++ jpg_view_folder/Form1.Designer.cs
@@ -57,17 +57,19 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(33, 62);
+            this.textBox1.Location = new System.Drawing.Point(44, 78);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(428, 19);
+            this.textBox1.Size = new System.Drawing.Size(569, 22);
             this.textBox1.TabIndex = 0;
             this.textBox1.Visible = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(31, 12);
+            this.button1.Location = new System.Drawing.Point(41, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(134, 29);
+            this.button1.Size = new System.Drawing.Size(179, 36);
             this.button1.TabIndex = 1;
             this.button1.Text = "写真指定";
             this.button1.UseVisualStyleBackColor = true;
@@ -75,9 +77,10 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(60, 118);
+            this.pictureBox1.Location = new System.Drawing.Point(80, 148);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(306, 352);
+            this.pictureBox1.Size = new System.Drawing.Size(408, 440);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -85,9 +88,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(653, 1);
+            this.button2.Location = new System.Drawing.Point(871, 1);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(252, 28);
+            this.button2.Size = new System.Drawing.Size(336, 35);
             this.button2.TabIndex = 3;
             this.button2.Text = "リスト";
             this.button2.UseVisualStyleBackColor = true;
@@ -97,18 +101,20 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(850, 76);
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(1133, 95);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(270, 556);
+            this.listBox1.Size = new System.Drawing.Size(359, 694);
             this.listBox1.TabIndex = 4;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(630, 638);
+            this.button3.Location = new System.Drawing.Point(887, 725);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(99, 26);
+            this.button3.Size = new System.Drawing.Size(132, 32);
             this.button3.TabIndex = 5;
             this.button3.Text = "コメント書き込み";
             this.button3.UseVisualStyleBackColor = true;
@@ -116,79 +122,83 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(33, 76);
+            this.textBox2.Location = new System.Drawing.Point(44, 95);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(428, 19);
+            this.textBox2.Size = new System.Drawing.Size(569, 22);
             this.textBox2.TabIndex = 6;
             this.textBox2.Visible = false;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(33, 638);
+            this.textBox3.Location = new System.Drawing.Point(80, 725);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(592, 19);
+            this.textBox3.Size = new System.Drawing.Size(788, 87);
             this.textBox3.TabIndex = 7;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(17, 816);
+            this.textBox4.Location = new System.Drawing.Point(23, 1020);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(589, 19);
+            this.textBox4.Size = new System.Drawing.Size(784, 22);
             this.textBox4.TabIndex = 7;
             this.textBox4.Visible = false;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(232, 10);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Location = new System.Drawing.Point(309, 12);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(72, 19);
+            this.textBox5.Size = new System.Drawing.Size(95, 22);
             this.textBox5.TabIndex = 8;
             this.textBox5.Text = "300";
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(232, 31);
-            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox6.Location = new System.Drawing.Point(309, 39);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(72, 19);
+            this.textBox6.Size = new System.Drawing.Size(95, 22);
             this.textBox6.TabIndex = 9;
             this.textBox6.Text = "400";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(184, 12);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(245, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 12);
+            this.label1.Size = new System.Drawing.Size(46, 15);
             this.label1.TabIndex = 10;
             this.label1.Text = "size X";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(184, 32);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(245, 40);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 12);
+            this.label2.Size = new System.Drawing.Size(46, 15);
             this.label2.TabIndex = 11;
             this.label2.Text = "size Y";
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(496, 118);
+            this.pictureBox2.Location = new System.Drawing.Point(661, 148);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(288, 352);
+            this.pictureBox2.Size = new System.Drawing.Size(384, 440);
             this.pictureBox2.TabIndex = 12;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(543, 12);
+            this.button4.Location = new System.Drawing.Point(724, 15);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(83, 29);
+            this.button4.Size = new System.Drawing.Size(111, 36);
             this.button4.TabIndex = 13;
             this.button4.Text = "ネガポジ変換";
             this.button4.UseVisualStyleBackColor = true;
@@ -196,33 +206,36 @@
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(543, 62);
+            this.textBox7.Location = new System.Drawing.Point(724, 78);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(50, 19);
+            this.textBox7.Size = new System.Drawing.Size(65, 22);
             this.textBox7.TabIndex = 14;
             this.textBox7.Visible = false;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(604, 62);
+            this.textBox8.Location = new System.Drawing.Point(805, 78);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(50, 19);
+            this.textBox8.Size = new System.Drawing.Size(65, 22);
             this.textBox8.TabIndex = 15;
             this.textBox8.Visible = false;
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(665, 62);
+            this.textBox9.Location = new System.Drawing.Point(887, 78);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(50, 19);
+            this.textBox9.Size = new System.Drawing.Size(65, 22);
             this.textBox9.TabIndex = 16;
             this.textBox9.Visible = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1028, 599);
+            this.ClientSize = new System.Drawing.Size(1371, 831);
             this.Controls.Add(this.textBox9);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.textBox7);
@@ -241,6 +254,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "メモメモjpg";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
